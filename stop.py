a62fe42f10c2adeb7114e801ff60133190dbb328
ErianArcher <jungg1996@gmail.com>
2019-11-11 21:31:16 +0800

    Copy the logs back.

--- stop.py
+++ stop.py
@@ -9,6 +9,7 @@ stopper_parser.add_argument('--hostnum', dest='hostnum', type=int, required=True
 
 stopper_args = stopper_parser.parse_args()
 scheduler_container_name = "scheduler"
+akka_log_fname = "akka_log.log"
 
 def get_host_container_name(hostNO):
     prefix = "mininet-"
@@ -27,6 +28,12 @@ def stop_scala_program(container_name):
         call(["docker exec -it " + container_name + " kill -9 " + line], shell=True)
     psef = call(["docker exec -it " + container_name + " ps -ef"], shell=True)
 
+def cp_log(container_name):
+    log_folder = "logs"
+    log_file_name_in_local = container_name + "_log.log"
+    call(["docker cp " + container_name + ":/" + akka_log_fname + " " +
+          os.path.join(log_folder,log_file_name_in_local)], shell=True)
+
 if __name__ == '__main__':
     if os.getuid() != 0:
         logging.debug("You are NOT root")
@@ -34,5 +41,8 @@ if __name__ == '__main__':
         hostNum = stopper_args.hostnum
 
         for hostNO in range(1, hostNum+1):
-            stop_scala_program(get_host_container_name(hostNO))
+            host_container_name = get_host_container_name(hostNO)
+            stop_scala_program(host_container_name)
+            cp_log(host_container_name)
         stop_scala_program(scheduler_container_name)
+        cp_log(scheduler_container_name)
